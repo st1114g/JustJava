6a77fbac93002f4d9c88d730651328bc8dd5bffd
st1114g <st1114g@gmail.com>
2017-10-27 21:20:07 -0400

    stuff

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -37,12 +37,12 @@ public class MainActivity extends AppCompatActivity {
     /** triggers the + button */
     public void increment(View view) {
         quantity = quantity + 1;
-        display(quantity);
+        displayQuantity(quantity);
     }
     /** triggers the - button */
     public void decrement(View view) {
         quantity = quantity - 1;
-        display(quantity);
+        displayQuantity (quantity);
     }
 
     public void submitOrder(View view) {
@@ -55,7 +55,7 @@ public class MainActivity extends AppCompatActivity {
     /**
      * This method displays the given quantity value on the screen.
      */
-    private void display(int number) {
+    private void displayQuantity(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
     }
